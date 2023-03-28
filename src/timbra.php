bd9f05d154a14c3c0d601940fa7060fd41f9538e
mgamboa <gamboa.martin@gmail.com>
2023-03-28 15:41:58 -0600

    v2.8.0 Se integra cancelacion

--- src/timbra.php
+++ src/timbra.php
@@ -17,6 +17,84 @@ class timbra{
 
     }
 
+    final public function cancela(string $motivo_cancelacion, string $rfc_emisor, string $uuid, string $pac_prov='', $uuid_sustitucion = ''){
+        $pac = new pac();
+        $keys = array('ruta_pac','usuario_integrador');
+        $valida = $this->valida->valida_existencia_keys(keys: $keys,registro:  $pac);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar pac',data: $valida);
+        }
+
+        if($motivo_cancelacion === '01'){
+            if($uuid_sustitucion === ''){
+                return $this->error->error(mensaje: 'Error uuid_sustitucion debe existir',data: $uuid_sustitucion);
+            }
+        }
+
+        $ws= $pac->ruta_pac;
+        $usuario_int = $pac->usuario_integrador;
+        $timbra_rs = 'CancelaCFDI40';
+        $aplica_params = true;
+        $tipo_entrada = 'xml';
+
+        if($pac_prov!==''){
+            $ws= $pac->pac->$pac_prov->ruta;
+            $usuario_int = $pac->pac->$pac_prov->pass;
+            $timbra_rs = $pac->pac->$pac_prov->timbra_rs;
+            $aplica_params = $pac->pac->$pac_prov->aplica_params;
+            $tipo_entrada = $pac->pac->$pac_prov->tipo_entrada;
+        }
+        $params = array();
+
+        $params['usuarioIntegrador'] = $usuario_int;
+        $params['rfcEmisor'] = $rfc_emisor;
+        //$params['rfcReceptor'] = $rfc_receptor;
+        $params['folioUUID'] = strtoupper(trim($uuid));
+        $params['motivoCancelacion'] = $motivo_cancelacion;
+        $params['folioUUIDSustitucion'] = $uuid_sustitucion;
+
+        try {
+            if($aplica_params) {
+                $client = new SoapClient($ws, $params);
+            }
+            else{
+                $client = new SoapClient($ws);
+            }
+        }
+        catch (Throwable $e){
+            return $this->error->error('Error al timbrar',array($e,$params));
+        }
+
+        if($aplica_params){
+            $response = $client->__soapCall($timbra_rs, array('parameters' => $params));
+
+        }
+
+        $result = $response->CancelaCFDI40Result->anyType;
+
+
+        $tipo_resultado = $result[0];
+        $cod_mensaje = $result[1];
+        $mensaje = $result[2];
+        $cod_error = $result[6];
+        $mensaje_error = $result[7];
+        $salida = $result[8];
+
+
+        $data = new stdClass();
+        $data->response = $response;
+        $data->result = $result;
+        $data->tipo_resultado = $tipo_resultado;
+        $data->cod_mensaje = $cod_mensaje;
+        $data->mensaje = $mensaje;
+        $data->cod_error = $cod_error;
+        $data->mensaje_error = $mensaje_error;
+        $data->salida = $salida;
+
+
+        return $data;
+    }
+
     public function timbra(string $contenido_xml, string $id_comprobante = '', string $pac_prov=''): array|stdClass
     {
 
