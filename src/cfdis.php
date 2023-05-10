8f2d93d98303bfd222019a8a6db87ffd85147f13
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-05-10 15:25:55 -0600

    v2.36.0 Se ajusta haberes

--- src/cfdis.php
+++ src/cfdis.php
@@ -447,6 +447,7 @@ class cfdis{
             return $this->error->error(mensaje: 'Error al asignar nodo', data: $nodo_nominas);
         }
 
+        
         $nodo_nominas_receptor = (new nomina())->nodo_nominas_receptor(nodo_nominas: $nodo_nominas, nomina: $nomina_, xml: $xml);
         if (errores::$error) {
             return $this->error->error(mensaje: 'Error al asignar nodo', data: $nodo_nominas_receptor);
